430056ebd1594f8103dd0fd25ed8d777c414d8ac
GreenSpaceman22 <titus-kovalik@outlook.com>
2024-04-01 13:04:18 -0700

    filled in point load class with constructor and calculations.

--- src/main/java/com/example/StudFinder/Model/HouseSubComponents/FloorSubComponents/WallSubComponents/PointLoads.java
+++ src/main/java/com/example/StudFinder/Model/HouseSubComponents/FloorSubComponents/WallSubComponents/PointLoads.java
@@ -2,4 +2,27 @@ package com.example.StudFinder.Model.HouseSubComponents.FloorSubComponents.WallS
 
 public class PointLoads {
 
+    private int lumberNumber;
+    private final int codeMultiplier = 3;
+
+    public PointLoads(int pointLoadCount) {
+        pointLoadCalculator(pointLoadCount);
+    }
+
+    private void pointLoadCalculator(int pointLoadCounter) {
+        int lumberNewCount = pointLoadCounter * getCodeMultiplier();
+        setLumberNumber(lumberNewCount);
+    }
+
+    public int getLumberNumber() {
+        return lumberNumber;
+    }
+
+    public void setLumberNumber(int lumberNumber) {
+        this.lumberNumber = lumberNumber;
+    }
+
+    public int getCodeMultiplier() {
+        return codeMultiplier;
+    }
 }
