9ef0341c3fe485aac5936f120ee9068852bee9e2
GreenSpaceman22 <titus-kovalik@outlook.com>
2024-04-02 16:56:13 -0700

    added better testing examples, and more todos

--- src/main/java/com/example/StudFinder/Controller/AppController.java
+++ src/main/java/com/example/StudFinder/Controller/AppController.java
@@ -1,16 +1,11 @@
 package com.example.StudFinder.Controller;
 
 import com.example.StudFinder.Model.House;
-import com.example.StudFinder.Model.HouseSubComponents.Floor;
 import com.example.StudFinder.Model.HouseSubComponents.FloorSubComponents.Wall;
 import com.example.StudFinder.Repository.HouseRepo;
-import com.example.StudFinder.supportEngines.ComponentLoader;
 import com.example.StudFinder.supportEngines.EndPointSupportEngine;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
 
 @RestController
 class AppController {
@@ -37,7 +32,10 @@ class AppController {
     }
 
     @RequestMapping(value = "/addNewFloor", method = RequestMethod.POST)
-    public House createFloor(@RequestBody double height, House house) {
+    public House createFloor(@RequestBody String heightStr, House house) {
+        System.out.println("height = ");
+        System.out.println("house = " + house);
+        double height = Double.parseDouble(heightStr);
         EndPointSupportEngine.addNewFloor(height, house);
         return house;
     }
