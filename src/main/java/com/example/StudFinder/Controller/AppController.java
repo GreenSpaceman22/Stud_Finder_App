f14e7b51b47fc7a6a8528e791981437b0b559678
GreenSpaceman22 <titus-kovalik@outlook.com>
2024-04-02 13:46:45 -0700

    added EndPointSupportEngine class to handle the incoming data to turn into objects, and end points now return a house object.

--- src/main/java/com/example/StudFinder/Controller/AppController.java
+++ src/main/java/com/example/StudFinder/Controller/AppController.java
@@ -1,10 +1,16 @@
 package com.example.StudFinder.Controller;
 
 import com.example.StudFinder.Model.House;
+import com.example.StudFinder.Model.HouseSubComponents.Floor;
+import com.example.StudFinder.Model.HouseSubComponents.FloorSubComponents.Wall;
 import com.example.StudFinder.Repository.HouseRepo;
+import com.example.StudFinder.supportEngines.ComponentLoader;
+import com.example.StudFinder.supportEngines.EndPointSupportEngine;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 class AppController {
@@ -12,23 +18,34 @@ class AppController {
     @Autowired
     HouseRepo repo;
 
-    @GetMapping("/")
-    public String render() {
-        return "Ah, yes, General Kenobi";
-        // change return String of HTML file thats been read? I guess.
-        //TODO: read up on how to read an HTML doc into a string format, make a method and return that to the web PORT 8080
-    }
-
+    // Below is alternate endpoint design.
     @PostMapping("/addHouse")
     public String addHouse(@RequestBody House house){
         repo.save(house);
         return "saved house";
     }
 
-    @RequestMapping(value = "/storeThing", method = RequestMethod.POST)
-    public String storeThing(@RequestBody House thing){
-        repo.save(thing);
+    @RequestMapping(value = "/storeHouse", method = RequestMethod.POST)
+    public String storeThing(@RequestBody House house){
+        repo.save(house);
         return "saved house";
+    }
+
+    @RequestMapping(value = "/startNewHouse", method = RequestMethod.POST)
+    public House createHouse(@RequestBody String address) {
+        return new House(address);
+    }
+
+    @RequestMapping(value = "/addNewFloor", method = RequestMethod.POST)
+    public House createFloor(@RequestBody double height, House house) {
+        EndPointSupportEngine.addNewFloor(height, house);
+        return house;
+    }
+
+    @RequestMapping(value = "/addNewWall", method = RequestMethod.POST)
+    public House createWall(@RequestBody Wall wall, House house, int floorNumber) {
+        EndPointSupportEngine.addNewWall(wall, house, floorNumber);
+        return house;
     }
 
 }
---
+++ src/main/java/com/example/StudFinder/supportEngines/EndPointSupportEngine.java
@@ -0,0 +1,21 @@
+package com.example.StudFinder.supportEngines;
+
+import com.example.StudFinder.Model.House;
+import com.example.StudFinder.Model.HouseSubComponents.Floor;
+import com.example.StudFinder.Model.HouseSubComponents.FloorSubComponents.Wall;
+
+import java.util.ArrayList;
+
+public class EndPointSupportEngine {
+
+    public static void addNewWall(Wall wall, House house, int floorNumber) {
+        ArrayList<Floor> floors = house.getFloors();
+        ComponentLoader.loadWall(floors.get(floorNumber - 1), wall);
+        house.setFloors(floors);
+    }
+    public static void addNewFloor(double height, House house) {
+        Floor floor = new Floor(height);
+        ComponentLoader.loadFloor(house, floor);
+    }
+
+}
